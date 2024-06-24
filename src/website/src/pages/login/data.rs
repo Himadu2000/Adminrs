764c3f3c54e3ef62cfc512cadc05329ec37952ba
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-23 21:47:04 -0700

    Update data.rs

--- src/website/src/pages/login/data.rs
+++ src/website/src/pages/login/data.rs
@@ -1,6 +1,7 @@
-use leptos::{ReadSignal, Resource};
+use leptos::{html::Input, NodeRef, ReadSignal, Resource};
 
 pub struct Data {
+    pub input: NodeRef<Input>,
     pub value: ReadSignal<i8>,
     pub text: Resource<(), String>,
 }
