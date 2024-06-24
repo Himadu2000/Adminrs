489350317741f7b0b11183e53b25de0b6db5dba0
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-23 21:45:15 -0700

    Update view.rs

--- src/website/src/pages/login/view.rs
+++ src/website/src/pages/login/view.rs
@@ -6,18 +6,16 @@ use leptos::{
 use leptos_meta::*;
 
 #[component]
-pub fn View<E1, E2, E3>(data: Data, events: (E1, E2, E3)) -> impl IntoView
+pub fn View<E1>(data: Data, events: (E1)) -> impl IntoView
 where
-    E1: Fn(MouseEvent) + 'static,
-    E2: Fn(MouseEvent) + 'static,
-    E3: Fn(SubmitEvent),
+    E1: Fn(SubmitEvent),
 {
     view! {
         <Title text="Home"/>
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
-                <form on:submit=events.3>
+                <form on:submit=events.1>
                 <input type="text" node_ref=input_element class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
                 <input type="submit" value="Log In" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"/>
             </form>
