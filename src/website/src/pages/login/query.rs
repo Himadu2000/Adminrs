8efaa11e58127c6b93e2981a958ef52e9dfa3d79
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-24 00:56:31 -0700

    Update query.rs

--- src/website/src/pages/login/query.rs
+++ src/website/src/pages/login/query.rs
@@ -1,9 +1,14 @@
 use crate::pages::schema;
 use cynic::QueryFragment;
 
+#[derive(cynic::QueryVariables)]
+pub struct Variables {
+    pub email: String,
+}
+
 #[derive(cynic::QueryFragment, Debug)]
-#[cynic(graphql_type = "Mutation")]
+#[cynic(graphql_type = "Mutation", variables = "Variables")]
 pub struct MyMutation {
-    #[arguments(email: "")]
+    #[arguments(email: $email)]
     pub login: String,
 }
