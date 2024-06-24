8d388b4044de6ef03f9d0dfe875dfa554347cc62
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-23 21:11:14 -0700

    Create create.rs

---
+++ src/website/src/pages/product/create.rs
@@ -0,0 +1,53 @@
+use crate::pages::schema;
+use cynic::QueryFragment;
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Mutation")]
+pub struct MyMutation {
+    #[arguments(data: {  })]
+    pub create_product: Vec<ProductRecord>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct ProductRecord {
+    pub id: String,
+}
+
+// Update
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Mutation")]
+pub struct MyMutation {
+    #[arguments(data: {  }, id: "")]
+    pub update_product: Option<ProductRecord>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct ProductRecord {
+    pub id: String,
+}
+
+// Delete
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Mutation")]
+pub struct MyMutation {
+    #[arguments(id: "")]
+    pub delete_product: Option<ProductRecord>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct ProductRecord {
+    pub id: String,
+}
+
+// Delete Image
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Mutation")]
+pub struct MyMutation {
+    #[arguments(id: "", index: 10)]
+    pub delete_image: Option<ProductRecord>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct ProductRecord {
+    pub id: String,
+}
