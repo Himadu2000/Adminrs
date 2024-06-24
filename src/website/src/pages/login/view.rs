98370aec97662c9fd295a2ecb02ef3d936101e2c
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-23 21:52:40 -0700

    Update view.rs

--- src/website/src/pages/login/view.rs
+++ src/website/src/pages/login/view.rs
@@ -16,7 +16,7 @@ where
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
                 <form on:submit=events>
-                <input type="text" node_ref=input_element class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
+                <input type="text" node_ref=data.form class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
                 <input type="submit" value="Log In" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"/>
             </form>
                 </div>
