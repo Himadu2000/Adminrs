60291a32b5699b79d9ce2db99021202ab82b3680
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-23 21:04:30 -0700

    Update query.rs

--- src/website/src/pages/products/query.rs
+++ src/website/src/pages/products/query.rs
@@ -1,8 +1,18 @@
 use crate::pages::schema;
 use cynic::QueryFragment;
 
-#[derive(QueryFragment, Debug)]
+#[derive(cynic::QueryFragment, Debug)]
 #[cynic(graphql_type = "Query")]
-pub struct UnnamedQuery {
-    pub status: String,
+pub struct MyQuery {
+    pub get_products: IntConnection,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct IntConnection {
+    pub edges: Vec<IntEdge>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+pub struct IntEdge {
+    pub node: i32,
 }
