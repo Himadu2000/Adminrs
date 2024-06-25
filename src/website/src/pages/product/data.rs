18a391c0ca7b982ba5dd17bd49975bb3e31c691d
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 05:52:04 -0700

    Update data.rs

--- src/website/src/pages/product/data.rs
+++ src/website/src/pages/product/data.rs
@@ -4,5 +4,5 @@ use leptos::{ReadSignal, Resource};
 pub struct Data {
     pub value: ReadSignal<i8>,
     pub product: ReadSignal<ProductRecord>,
-    pub text: Resource<(), String>,
+    pub text: Resource<ProductRecord, String>,
 }
