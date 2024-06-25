3fe5356e233dc0c718dbd6f9938a9ed44aeafcde
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 06:32:15 -0700

    Update view.rs

--- src/website/src/pages/product/view.rs
+++ src/website/src/pages/product/view.rs
@@ -42,6 +42,8 @@ where
                 <input type="number" placeholder="Enter Username" name="uname" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
                 <input type="number" placeholder="Enter Username" name="uname" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
 
+                <input type="number" placeholder="Enter Username" name="uname" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
+
                 <input type="submit" value="Log In" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"/>
             </form>
 
