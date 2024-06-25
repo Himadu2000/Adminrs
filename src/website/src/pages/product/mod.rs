74bb061e63e2e5ec3b48ea88e816ec58e060a4dd
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 05:42:22 -0700

    Update mod.rs

--- src/website/src/pages/product/mod.rs
+++ src/website/src/pages/product/mod.rs
@@ -65,7 +65,7 @@ pub fn Product() -> impl IntoView {
         }
     });
 
-    let update = create_action(move |product: &(String, ProductInput)| {
+    let update = create_action(move |input: &(String, ProductInput)| {
         let product = product.to_owned();
 
         async move {
