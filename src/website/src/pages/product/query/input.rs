9b3714b5abf425a44d5f882e32d27a843369a4b6
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 04:19:14 -0700

    Create input.rs

---
+++ src/website/src/pages/product/query/input.rs
@@ -0,0 +1,95 @@
+pub mod crud;
+
+use crate::pages::schema;
+use cynic::QueryFragment;
+
+#[derive(cynic::QueryFragment, cynic::InputObject, Debug)]
+#[cynic(graphql_type = "Query")]
+pub struct UnnamedQuery {
+    #[arguments(id: "")]
+    pub get_product: Option<ProductRecord>,
+}
+
+#[derive(cynic::QueryFragment, cynic::InputObject, cynic::QueryVariables, Debug)]
+pub struct ProductRecord {
+    pub attributes: Vec<Attribute>,
+    pub code: Option<String>,
+    pub cost_price: Option<f64>,
+    pub date_sale_from: String,
+    pub date_sale_to: String,
+    pub date_stock_expected: String,
+    pub description: Option<String>,
+    pub dimensions: Option<Dimensions>,
+    pub discontinued: bool,
+    pub enabled: bool,
+    pub id: String,
+    pub images: Vec<Image>,
+    pub meta_description: Option<String>,
+    pub meta_title: Option<String>,
+    pub name: Option<String>,
+    pub options: Vec<ProductOption>,
+    pub position: i32,
+    pub prices: Vec<f64>,
+    pub quantity_inc: i32,
+    pub quantity_min: i32,
+    pub regular_price: f64,
+    pub sale_price: Option<f64>,
+    pub sku: Option<String>,
+    pub slug: Option<String>,
+    pub stock_backorder: bool,
+    pub stock_preorder: bool,
+    pub stock_quantity: i32,
+    pub stock_tracking: bool,
+    pub tags: Vec<String>,
+    pub tax_class: Option<String>,
+    pub variants: Vec<Variant>,
+    pub weight: Option<f64>,
+}
+
+#[derive(cynic::QueryFragment, cynic::InputObject, Debug)]
+pub struct Variant {
+    pub options: String,
+    pub price: f64,
+    pub sku: Option<String>,
+    pub stock_quantity: i32,
+    pub weight: Option<f64>,
+}
+
+#[derive(cynic::QueryFragment, cynic::InputObject, Debug)]
+pub struct ProductOption {
+    pub control: ProductOptionControl,
+    pub name: String,
+    pub position: i32,
+    pub required: bool,
+    pub values: Vec<Value>,
+}
+
+#[derive(cynic::QueryFragment, cynic::InputObject, Debug)]
+pub struct Value {
+    pub name: String,
+}
+
+#[derive(cynic::QueryFragment, cynic::InputObject, Debug)]
+pub struct Image {
+    pub alt: Option<String>,
+    pub file: String,
+    pub position: i32,
+}
+
+#[derive(cynic::QueryFragment, cynic::InputObject, Debug)]
+pub struct Dimensions {
+    pub height: f64,
+    pub length: f64,
+    pub width: f64,
+}
+
+#[derive(cynic::QueryFragment, cynic::InputObject, Debug)]
+pub struct Attribute {
+    pub name: String,
+    pub value: String,
+}
+
+#[derive(cynic::Enum, Clone, Copy, Debug)]
+pub enum ProductOptionControl {
+    Select,
+}
