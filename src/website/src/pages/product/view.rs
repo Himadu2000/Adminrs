d0f4b9af0238fca120858f9775c9f56e709118f0
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-26 03:52:36 -0700

    Update view.rs

--- src/website/src/pages/product/view.rs
+++ src/website/src/pages/product/view.rs
@@ -52,7 +52,7 @@ where
 
                 <input type="number" placeholder="Enter Position" name="position" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
 
-                <input type="submit" value="Log In" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"/>
+                <input type="submit" value="Save" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"/>
             </form>
 
             <Toggle state=data.state set_state=data.set_state/>
