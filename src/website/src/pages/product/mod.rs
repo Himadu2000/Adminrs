14373b72ef6c9b0e129f52995abbd3f6601a12a8
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-26 05:33:31 -0700

    Update mod.rs

--- src/website/src/pages/product/mod.rs
+++ src/website/src/pages/product/mod.rs
@@ -29,7 +29,7 @@ pub fn Product() -> impl IntoView {
     let (value, set_value) = create_signal::<i8>(0);
     let (selected_product, set_selected_product) =
         create_signal::<String>(String::from("zinftrowowh62zcp64fj"));
-    let form_values = create_signal::<HashMap<String, String>>(HashMap::new());
+    let form_values = create_rw_signal::<HashMap<String, String>>(HashMap::new());
     let (state, set_state) = create_signal(false);
 
     let id = move || {
