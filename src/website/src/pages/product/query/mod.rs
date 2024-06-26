4e16aa60146401d2aa6e89c09c9994fc2ca76b86
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-26 08:49:07 -0700

    Update mod.rs

--- src/website/src/pages/product/query/mod.rs
+++ src/website/src/pages/product/query/mod.rs
@@ -13,7 +13,7 @@ pub struct Variables {
 #[cynic(graphql_type = "Query", variables = "Variables")]
 pub struct UnnamedQuery {
     #[arguments(id: $id)]
-    pub get_product: ProductDbRecord,
+    pub get_product: ProductRecord,
 }
 
 #[derive(Clone, cynic::QueryFragment, Debug, Serialize)]
@@ -22,7 +22,7 @@ pub struct ProductDbRecord {
 }
 
 #[derive(Clone, cynic::QueryFragment, Debug, Serialize)]
-pub struct Product {
+pub struct ProductRecord {
     pub attributes: Vec<Attribute>,
     pub code: Option<String>,
     pub cost_price: Option<f64>,
