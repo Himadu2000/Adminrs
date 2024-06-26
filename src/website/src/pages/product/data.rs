80a2b06db63fbf6fb58d03bf0c7c4e5210612d5d
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-25 22:17:12 -0700

    Update data.rs

--- src/website/src/pages/product/data.rs
+++ src/website/src/pages/product/data.rs
@@ -1,9 +1,9 @@
-use super::query::ProductRecord;
+use super::query::ProductDbRecord;
 use leptos::{ReadSignal, Resource, WriteSignal};
 
 pub struct Data {
     pub value: ReadSignal<i8>,
     pub state: ReadSignal<bool>,
     pub set_state: WriteSignal<bool>,
-    pub product: Resource<(), ProductRecord>,
+    pub product: Resource<(), ProductDbRecord>,
 }
