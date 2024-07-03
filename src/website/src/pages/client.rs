1b3f485c1635b24a08c30d111156b53637dd182c
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-03 09:59:30 -0700

    Update client.rs

--- src/website/src/pages/client.rs
+++ src/website/src/pages/client.rs
@@ -76,3 +76,40 @@ pub async fn upload_client(form: Form) -> String {
         .await
         .unwrap()
 }
+
+//? Following   
+pub async fn _env_client<UnnamedQuery>(operation: impl Serialize) -> Option<UnnamedQuery>
+where
+    UnnamedQuery: QueryFragment + for<'a> Deserialize<'a>,
+{
+    let (store_id, _) = create_signal(var("STORE_ID"));
+    let (flag, _, _) = use_local_storage::<String, FromToStringCodec>("token");
+    let (local_store_id, set_store_id, _) =
+        use_local_storage::<String, FromToStringCodec>("store_id");
+
+    //? Set environment variable to local storage for reqwest client.
+    let store_id_env = store_id
+        .get_untracked()
+        // .expect("STORE_ID environment variable not provided...!")
+        .unwrap_or("n6whojbatetnjgn85kyx".to_owned());
+
+    set_store_id.set(store_id_env);
+
+    Client::new()
+        .post("http://127.0.0.1:8000/graphql")
+        .json(&operation)
+        .header("Authorization", flag.get_untracked())
+        .header(
+            "store_id",
+            store_id
+                .get_untracked()
+                .unwrap_or(local_store_id.get_untracked()),
+        )
+        .send()
+        .await
+        .unwrap()
+        .json::<GraphQlResponse<UnnamedQuery>>()
+        .await
+        .unwrap()
+        .data
+}
