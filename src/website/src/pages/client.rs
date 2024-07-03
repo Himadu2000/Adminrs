a0a6436710fd8d8cb11c53001e01582f30ce2cfe
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-03 10:03:21 -0700

    Update client.rs

--- src/website/src/pages/client.rs
+++ src/website/src/pages/client.rs
@@ -14,14 +14,7 @@ where
     UnnamedQuery: QueryFragment + for<'a> Deserialize<'a>,
 {
     let (flag, _, _) = use_local_storage::<String, FromToStringCodec>("token");
-    let (local_store_id, set_store_id, _) =
-        use_local_storage::<String, FromToStringCodec>("store_id");
-
-    //? Set environment variable to local storage for reqwest client.
-    let store_id_env = store_id
-        .get_untracked()
-        // .expect("STORE_ID environment variable not provided...!")
-        .unwrap_or("store:obgsketriakxn1wh3q2e".to_owned());
+    let (store_id, _, _) = use_local_storage::<String, FromToStringCodec>("store_id");
 
     set_store_id.set(store_id_env);
 
@@ -29,12 +22,7 @@ where
         .post("http://127.0.0.1:8000/graphql")
         .json(&operation)
         .header("Authorization", flag.get_untracked())
-        .header(
-            "store_id",
-            store_id
-                .get_untracked()
-                .unwrap_or(local_store_id.get_untracked()),
-        )
+        .header("store_id", store_id.get_untracked())
         .send()
         .await
         .unwrap()
@@ -45,29 +33,14 @@ where
 }
 
 pub async fn upload_client(form: Form) -> String {
-    let (store_id, _) = create_signal(var("STORE_ID"));
     let (flag, _, _) = use_local_storage::<String, FromToStringCodec>("token");
-    let (local_store_id, set_store_id, _) =
-        use_local_storage::<String, FromToStringCodec>("store_id");
-
-    //? Set environment variable to local storage for reqwest client.
-    let store_id_env = store_id
-        .get_untracked()
-        // .expect("STORE_ID environment variable not provided...!")
-        .unwrap_or("n6whojbatetnjgn85kyx".to_owned());
-
-    set_store_id.set(store_id_env);
+    let (store_id, _, _) = use_local_storage::<String, FromToStringCodec>("store_id");
 
     Client::new()
         .post("http://127.0.0.1:8000/graphql")
         .multipart(form)
         .header("Authorization", flag.get_untracked())
-        .header(
-            "store_id",
-            store_id
-                .get_untracked()
-                .unwrap_or(local_store_id.get_untracked()),
-        )
+        .header("store_id", store_id.get_untracked())
         .send()
         .await
         .unwrap()
