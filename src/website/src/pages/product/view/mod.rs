fe479e71a980946d5b961e885d7f5ebb7f183564
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 04:55:17 -0700

    Update mod.rs

--- src/website/src/pages/product/view/mod.rs
+++ src/website/src/pages/product/view/mod.rs
@@ -1,6 +1,8 @@
+mod form;
 mod text;
 
 use super::data::Data;
+use form::Form;
 use leptonic::toggle::Toggle;
 use leptos::{
     ev::{MouseEvent, SubmitEvent},
@@ -41,18 +43,7 @@ where
                         on_submit.dispatch(());
                     }>
 
-                <TextInput input=form_values input_type="text" placeholder="Enter Product name *" name="name"/>
-                <TextInput input=form_values input_type="text" placeholder="Enter Slug" name="slug"/>
-                <p>"The “slug” is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens."</p>
-                <TextInput input=form_values input_type="text" placeholder="Enter Page title" name="meta_title"/>
-                <TextInput input=form_values input_type="text" placeholder="Enter Meta Description" name="meta_description"/>
-                <TextInput input=form_values input_type="text" placeholder="Enter Description" name="description"/>
-
-                <TextInput input=form_values input_type="number" placeholder="Enter Regular price ($)" name="regular_price"/>
-                <TextInput input=form_values input_type="number" placeholder="Enter Stock quantity" name="stock_quantity"/>
-                <TextInput input=form_values input_type="number" placeholder="Enter Weight (kg)" name="weight"/>
-
-                <TextInput input=form_values input_type="number" placeholder="Enter Position" name="position"/>
+                <Form input=form_values />
 
                 <input type="submit" value="Save" class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"/>
             </form>
