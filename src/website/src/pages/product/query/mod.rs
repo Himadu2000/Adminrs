9d499b05317e687667ae9c6880cdb2498af704fb
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 03:10:49 -0700

    Update mod.rs

--- src/website/src/pages/product/query/mod.rs
+++ src/website/src/pages/product/query/mod.rs
@@ -2,7 +2,7 @@ pub mod crud;
 pub mod input;
 
 use crate::pages::schema;
-use cynic::{Id, QueryFragment, QueryVariables};
+use cynic::{Enum, Id, QueryFragment, QueryVariables};
 use serde::Serialize;
 
 #[derive(QueryVariables)]
@@ -26,23 +26,23 @@ pub struct ProductRecord {
     pub meta_description: String,
     pub regular_price: f64,
     pub sale_price: f64,
-    // pub date_sale_from: Datetime,
-    // pub date_sale_to: Datetime,
+    pub date_sale_from: String,
+    pub date_sale_to: String,
     pub sku: String,
     pub stock_quantity: i32,
     pub weight: f64,
-    // pub date_stock_expected: Datetime,
+    pub date_stock_expected: String,
     pub stock_tracking: bool,
     pub stock_preorder: bool,
     pub stock_backorder: bool,
     pub discontinued: bool,
     pub enabled: bool,
     pub attributes: Vec<Attribute>,
-    // pub variants: Vec<VariantInput>,
-    // pub category_ids: Vec<Thing>,
+    pub variants: Vec<Variant>,
+    // pub category_ids: Vec<Id>,
     pub tags: Vec<String>,
     pub position: i32,
-    // pub related_products: Vec<Thing>,
+    // pub related_products: Vec<Id>,
     pub images: Vec<ImageOutput>,
 }
 
@@ -59,11 +59,24 @@ pub struct Attribute {
     pub value: String,
 }
 
+#[derive(Enum, Clone, Copy, Debug)]
+pub enum ProductOptionControl {
+    SELECT,
+}
+
+#[derive(Clone, QueryFragment, Debug, Serialize)]
+pub struct VariantOption {
+    pub name: String,
+    pub control: ProductOptionControl,
+    pub required: bool,
+    pub values: Vec<String>,
+}
+
 #[derive(Clone, QueryFragment, Debug, Serialize)]
 pub struct Variant {
     pub sku: String,
     pub price: f64,
     pub stock_quantity: i32,
     pub weight: f64,
-    // pub options: Vec<VariantOption>,
+    pub options: Vec<VariantOption>,
 }
