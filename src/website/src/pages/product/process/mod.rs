a0ee1a0ffc2ab2ad05b17ef8cf66e5e83a0a71c9
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 06:00:51 -0700

    Update mod.rs

--- src/website/src/pages/product/process/mod.rs
+++ src/website/src/pages/product/process/mod.rs
@@ -19,14 +19,15 @@ pub async fn get_product(
     form_values: RwSignal<HashMap<String, String>>,
 ) -> Option<ProductRecord> {
     let variables = Variables {
-        id: selected_product.into(),
+        id: selected_product.get().into(),
     };
 
     let data = client::<UnnamedQuery>(UnnamedQuery::build(variables)).await;
 
-    if let Some(with_data) = data {
-        let with_data = with_data.get_product;
-        let data_product = with_data.clone();
+    if let Some(with_product) = data {
+        let with_product = with_product.get_product;
+        let data_product = with_product.clone();
+
         form_values.update(|values| {
             values.insert(String::from("name"), data_product.name);
             values.insert(String::from("slug"), data_product.slug);
@@ -38,7 +39,7 @@ pub async fn get_product(
             values.insert(String::from("description"), data_product.description);
         });
 
-        return Some(with_data);
+        return Some(with_product);
     }
 
     None
