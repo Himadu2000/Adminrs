23f28139f97a3dd9ebb279bdf9c8fc699e897427
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 05:57:44 -0700

    Update mod.rs

--- src/website/src/pages/product/process/mod.rs
+++ src/website/src/pages/product/process/mod.rs
@@ -4,7 +4,7 @@ use super::query::{
         UpdateProduct, UpdateProductVariables,
     },
     input::ProductInput,
-    UnnamedQuery, Variables,
+    ProductRecord, UnnamedQuery, Variables,
 };
 use crate::pages::client::{client, upload_client, MutationBuilder, QueryBuilder};
 use js_sys::Uint8Array;
@@ -17,29 +17,32 @@ use thaw::FileList;
 pub async fn get_product(
     selected_product: ReadSignal<String>,
     form_values: RwSignal<HashMap<String, String>>,
-) {
+) -> Option<ProductRecord> {
     let variables = Variables {
         id: selected_product.get().into(),
     };
 
-    let data = client::<UnnamedQuery>(UnnamedQuery::build(variables))
-        .await
-        .unwrap()
-        .get_product;
+    let data = client::<UnnamedQuery>(UnnamedQuery::build(variables)).await;
 
-    let data_product = data.clone();
-    form_values.update(|values| {
-        values.insert(String::from("name"), data_product.name);
-        values.insert(String::from("slug"), data_product.slug);
-        values.insert(String::from("meta_title"), data_product.meta_title);
-        values.insert(
-            String::from("meta_description"),
-            data_product.meta_description,
-        );
-        values.insert(String::from("description"), data_product.description);
-    });
+    if let Some(with_product) = data {
+        let with_product = with_product.get_product;
+        let data_product = data.clone();
 
-    data
+        form_values.update(|values| {
+            values.insert(String::from("name"), data_product.name);
+            values.insert(String::from("slug"), data_product.slug);
+            values.insert(String::from("meta_title"), data_product.meta_title);
+            values.insert(
+                String::from("meta_description"),
+                data_product.meta_description,
+            );
+            values.insert(String::from("description"), data_product.description);
+        });
+
+        Some(with_product)
+    }
+
+    None
 }
 
 pub async fn create_product(product: &ProductInput) {
