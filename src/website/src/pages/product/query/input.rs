0ba3966e661095fb9e549db2170e7087dfef3ca6
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 03:24:46 -0700

    Update input.rs

--- src/website/src/pages/product/query/input.rs
+++ src/website/src/pages/product/query/input.rs
@@ -1,6 +1,7 @@
 use crate::pages::schema;
+use cynic::{Enum, InputObject};
 
-#[derive(Clone, cynic::InputObject, Debug, Default)]
+#[derive(Clone, InputObject, Debug, Default)]
 pub struct ProductInput {
     pub name: String,
     pub description: String,
@@ -9,44 +10,45 @@ pub struct ProductInput {
     pub meta_description: String,
     pub regular_price: f64,
     pub sale_price: f64,
-    // pub date_sale_from: Datetime,
-    // pub date_sale_to: Datetime,
+    // pub date_sale_from: String,
+    // pub date_sale_to: String,
     pub sku: String,
     pub stock_quantity: i32,
     pub weight: f64,
-    // pub date_stock_expected: Datetime,
+    // pub date_stock_expected: String,
     pub stock_tracking: bool,
     pub stock_preorder: bool,
     pub stock_backorder: bool,
     pub discontinued: bool,
     pub enabled: bool,
     pub attributes: Vec<AttributeInput>,
-    // pub variants: Vec<VariantInput>,
-    // pub category_ids: Vec<Thing>,
+    pub variants: Vec<VariantInput>,
+    // pub category_ids: Vec<Id>,
     pub tags: Vec<String>,
     pub position: i32,
-    // pub related_products: Vec<Thing>,
+    // pub related_products: Vec<Id>,
     // pub images: Vec<ImageInput>,
 }
 
-// #[derive(Clone, cynic::InputObject, Debug)]
+// #[derive(Clone, InputObject, Debug)]
 // pub struct ImageInput {
-//     pub alt: String,
 //     pub file: String,
+//     pub alt: String,
 // }
 
-#[derive(Clone, cynic::InputObject, Debug)]
+#[derive(Clone, InputObject, Debug)]
 pub struct AttributeInput {
     pub name: String,
     pub value: String,
 }
 
-#[derive(cynic::Enum, Clone, Copy, Debug)]
+#[derive(Enum, Clone, Copy, Debug)]
 pub enum ProductOptionControl {
-    Select,
+    SELECT,
 }
 
-#[derive(Clone, cynic::InputObject, Debug)]
+#[derive(Clone, InputObject, Debug)]
+// VariantOptionInput
 pub struct ProductOptionInput {
     pub name: String,
     pub control: ProductOptionControl,
@@ -54,7 +56,7 @@ pub struct ProductOptionInput {
     pub values: Vec<String>,
 }
 
-#[derive(Clone, cynic::InputObject, Debug)]
+#[derive(Clone, InputObject, Debug)]
 pub struct VariantInput {
     pub sku: String,
     pub price: f64,
