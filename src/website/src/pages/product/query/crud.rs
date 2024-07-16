b0e1f0656282ccf8e56273e1eaebaa9f7a69597b
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 02:39:00 -0700

    Update crud.rs

--- src/website/src/pages/product/query/crud.rs
+++ src/website/src/pages/product/query/crud.rs
@@ -1,32 +1,33 @@
 use super::input::ProductInput;
 use crate::pages::schema;
+use cynic::{Id, QueryFragment, QueryVariables};
 
 // Create
-#[derive(cynic::QueryVariables)]
+#[derive(QueryVariables)]
 pub struct CreateProductVariables {
     pub data: ProductInput,
 }
 
-#[derive(cynic::QueryFragment, Debug)]
+#[derive(QueryFragment, Debug)]
 #[cynic(graphql_type = "Mutation", variables = "CreateProductVariables")]
 pub struct CreateProduct {
     #[arguments(data: $data)]
     pub create_product: Vec<ProductRecord>,
 }
 
-#[derive(cynic::QueryFragment, Debug)]
+#[derive(QueryFragment, Debug)]
 pub struct ProductRecord {
-    pub id: String,
+    pub id: Id,
 }
 
 // Update
-#[derive(cynic::QueryVariables)]
+#[derive(QueryVariables)]
 pub struct UpdateProductVariables {
-    pub id: String,
+    pub id: Id,
     pub data: ProductInput,
 }
 
-#[derive(cynic::QueryFragment, Debug)]
+#[derive(QueryFragment, Debug)]
 #[cynic(graphql_type = "Mutation", variables = "UpdateProductVariables")]
 pub struct UpdateProduct {
     #[arguments(data: $data, id: $id)]
@@ -34,14 +35,26 @@ pub struct UpdateProduct {
 }
 
 // Delete
-#[derive(cynic::QueryVariables)]
+#[derive(QueryVariables)]
 pub struct DeleteProductVariables {
-    pub id: String,
+    pub id: Id,
 }
 
-#[derive(cynic::QueryFragment, Debug)]
+#[derive(QueryFragment, Debug)]
 #[cynic(graphql_type = "Mutation", variables = "DeleteProductVariables")]
 pub struct DeleteProduct {
     #[arguments(id: $id)]
     pub delete_product: Option<ProductRecord>,
+}
+
+// Products
+#[derive(QueryFragment, Debug)]
+#[cynic(graphql_type = "Query")]
+pub struct MyQuery {
+    pub get_products: ProductRecordConnection,
+}
+
+#[derive(QueryFragment, Debug)]
+pub struct ProductRecordConnection {
+    pub nodes: Vec<ProductRecord>,
 }
