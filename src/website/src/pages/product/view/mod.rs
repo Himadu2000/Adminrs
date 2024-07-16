0dac9dfc9a4cc98170ef33b7a3f14709fb5ae402
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 04:45:00 -0700

    Update mod.rs

--- src/website/src/pages/product/view/mod.rs
+++ src/website/src/pages/product/view/mod.rs
@@ -1,3 +1,5 @@
+mod text;
+
 use super::data::Data;
 use leptonic::toggle::Toggle;
 use leptos::{
@@ -6,26 +8,8 @@ use leptos::{
 };
 use leptos_meta::*;
 use std::collections::HashMap;
+use text::TextInput;
 use thaw::{FileList, Upload, UploadDragger};
-
-#[component]
-pub fn TextInput(
-    input: RwSignal<HashMap<String, String>>,
-    input_type: &'static str,
-    placeholder: &'static str,
-    name: &'static str,
-    class: &'static str,
-) -> impl IntoView {
-    view! {
-        <input type="text"
-        on:input=move |ev| {
-            let value=event_target_value(&ev);
-            input.update(|values|{ values.insert(name.to_string(), value); });
-        }
-        prop:value=input.get().get(&name.to_owned())
-        placeholder=placeholder name=name class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white"/>
-    }
-}
 
 #[component]
 pub fn View(
