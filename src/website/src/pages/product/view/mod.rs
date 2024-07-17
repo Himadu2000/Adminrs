9d8af334a7831743617aa1a8bf7774ddd867a8bc
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 21:23:39 -0700

    Update mod.rs

--- src/website/src/pages/product/view/mod.rs
+++ src/website/src/pages/product/view/mod.rs
@@ -13,9 +13,11 @@ use thaw::{FileList, Upload, UploadDragger};
 pub fn View(
     data: Data,
     form_values: RwSignal<HashMap<String, String>>,
-    custom_request: E2,
     on_submit: Action<(RwSignal<HashMap<String, String>>, Option<u8>), ()>,
     upload: Action<(FileList, Option<u8>), ()>,
+    delete: Action<(String, Option<u8>), String>,
+    set_selected_product: WriteSignal<String>,
+    set_store_id: WriteSignal<String>,
 ) -> impl IntoView
 where
     E1: Fn(SubmitEvent) + 'static,
@@ -26,10 +28,45 @@ where
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
-                    <p>"Product"</p>
+                <button on:click=move |_| set_store_id.set(String::from("bwg3p7nmdux1e44l6pub")) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                    "Fondtees.com"
+                </button>
+                <button on:click=move |_| set_store_id.set(String::from("vkeqqn66h6rz3vixcm5p")) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                    "Varietydoor.com"
+                </button>
                     <Suspense
         fallback=move || view! { <p>"Loading..."</p> }
     >
+    <For
+                    each=move || data.products.with(|value| <std::option::Option<Vec<std::string::String>> as Clone>::clone(&value).unwrap_or_default())
+                    key=|product| product.clone()
+                    children=move |product| {
+                        let producta= product.clone();
+                        let productb= product.clone();
+                        let productc= product;
+                        view! {
+                            <li>
+                                <button
+                                    on:click=move |_| set_selected_product.set(producta.clone())
+                                    class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"
+                                >
+                                    {productb}
+                                </button>
+                                <button
+                                    on:click=move |_| delete.dispatch(((productc.clone(), None)))
+                                    class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"
+                                >
+                                    "Remove"
+                                </button>
+                            </li>
+                        }
+                    }
+                />
+                </Suspense>
+
+                <Suspense
+                fallback=move || view! { <p>"Loading..."</p> }
+            >
         {move || {
             data.product.get()
                 .map(|product| view! {
