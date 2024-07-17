35d2775de1dc23bf79067c5c0cf74b154551bd92
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-17 13:40:51 -0700

    Update mod.rs

--- src/website/src/pages/product/view/mod.rs
+++ src/website/src/pages/product/view/mod.rs
@@ -75,7 +75,7 @@ pub fn View(
 
             <Upload multiple=true custom_request=move |file_list| {upload.dispatch((selected_product.get_untracked(), file_list));}>
         <UploadDragger><p class="text-black">"Click or drag a file to this area to upload"</p>
-        {product.map(|item| item.images.iter().map(|image| view! { <img src=format!("http://localhost:8000/{}",image.file.clone()) alt=image.alt.clone()/> }).collect_view())}
+        {product.map(|item| item.images.iter().map(|image| view! { <img src=format!("http://localhost:8000{}",image.file.clone()) alt=image.alt.clone()/> }).collect_view())}
         </UploadDragger>
     </Upload>
                     </div>
