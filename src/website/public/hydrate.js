66119e2ca866638f13cd221f2c17d613f2a4ae46
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-17 09:20:11 -0700

    Create hydrate.js

---
+++ src/website/public/hydrate.js
@@ -0,0 +1,18 @@
+function idle(c) {
+  if ("requestIdleCallback" in window) {
+    window.requestIdleCallback(c);
+  } else {
+    c();
+  }
+}
+idle(() => {
+  import("/pkg/website.js").then((mod) => {
+    mod.default("/pkg/_bg.wasm").then(() => {
+      for (let e of document.querySelectorAll("leptos-island")) {
+        let l = e.dataset.component;
+        mod["_island_" + l](e);
+      }
+      mod.hydrate();
+    });
+  });
+});
