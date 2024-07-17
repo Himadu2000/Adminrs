54c6ef5e1b603f9f1295a0e0b135824c5e5f3b0c
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-17 13:13:33 -0700

    Update process.rs

--- src/website/src/pages/products/process.rs
+++ src/website/src/pages/products/process.rs
@@ -2,12 +2,18 @@ use super::query::MyQuery;
 use crate::pages::client::{client, QueryBuilder};
 
 pub async fn get_products() -> Vec<String> {
-    client::<MyQuery>(MyQuery::build(()))
-        .await
-        .unwrap()
-        .get_products
-        .edges
-        .iter()
-        .map(|product| product.node.id.inner().to_owned())
-        .collect()
+    let data = client::<MyQuery>(MyQuery::build(())).await;
+
+    if let Some(products) = data {
+        let list = products
+            .get_products
+            .edges
+            .iter()
+            .map(|product| product.node.id.inner().to_owned())
+            .collect();
+
+        return list;
+    }
+
+    vec![]
 }
