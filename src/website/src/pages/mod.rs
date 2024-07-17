ebcea27d0b66248a29570a2d09fe88e756c36378
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-17 05:37:17 -0700

    Update mod.rs

--- src/website/src/pages/mod.rs
+++ src/website/src/pages/mod.rs
@@ -24,7 +24,7 @@ pub fn App() -> impl IntoView {
 
     view! {
         <Stylesheet id="leptos" href="/pkg/website.css"/>
-        <script src="/pkg/website.js" defer />
+        <script src="/pkg/website.js" defer></script>
         <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
         <Router>
             <Routes>
