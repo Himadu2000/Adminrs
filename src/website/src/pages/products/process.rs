48053a28ded012694f8c167f6a5afbe9f3ee1a2f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-16 20:42:43 -0700

    Update process.rs

--- src/website/src/pages/products/process.rs
+++ src/website/src/pages/products/process.rs
@@ -1,30 +1,13 @@
-use super::query::{MyQuery, ProductRecord};
-use crate::pages::client::{client, upload_client, MutationBuilder, QueryBuilder};
+use super::query::MyQuery;
+use crate::pages::client::{client, QueryBuilder};
 
-pub async fn get_products() -> Vec<ProductRecord> {
-    let variables = Variables {
-        id: selected_product.into(),
-    };
-
-    let data = client::<UnnamedQuery>(UnnamedQuery::build(variables)).await;
-
-    if let Some(with_product) = data {
-        let with_product = with_product.get_product;
-        let data_product = with_product.clone();
-
-        form_values.update(|values| {
-            values.insert(String::from("name"), data_product.name);
-            values.insert(String::from("slug"), data_product.slug);
-            values.insert(String::from("meta_title"), data_product.meta_title);
-            values.insert(
-                String::from("meta_description"),
-                data_product.meta_description,
-            );
-            values.insert(String::from("description"), data_product.description);
-        });
-
-        return Some(with_product);
-    }
-
-    None
+pub async fn get_products() -> Vec<String> {
+    client::<MyQuery>(MyQuery::build(()))
+        .await
+        .unwrap()
+        .get_products
+        .edges
+        .iter()
+        .map(|product| product.node.id.inner().to_owned())
+        .collect()
 }
