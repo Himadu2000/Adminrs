8f743c40b7c1eab73ad7faba625ff15232df804d
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-07-19 17:30:43 -0700

    Update mod.rs

--- src/website/src/pages/product/view/mod.rs
+++ src/website/src/pages/product/view/mod.rs
@@ -27,10 +27,10 @@ pub fn View(
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
-                <button on:click=move |_| set_store_id.set(String::from("rizzyik1zr9nsyeblujk")) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                <button on:click=move |_| set_store_id.set(String::from("mx8v8wibis5wewl4eq62")) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
                     "Fondtees.com"
                 </button>
-                <button on:click=move |_| set_store_id.set(String::from("4h75xs7hl1hep4p6kv4b")) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                <button on:click=move |_| set_store_id.set(String::from("mmzp08vulm6ijkqusk45")) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
                     "Varietydoor.com"
                 </button>
                 <button on:click=move |_| create.dispatch(()) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
